acf623525e9b801f951c1adff13d8a455c21f9ac
7jXlqS-hmpBUGsK <7jXlqS-hmpBUGsK@users.noreply.github.com>
2018-09-15 08:40:31 -0400

    change pointer cast type from int to intptr_t

--- doc_msgs.c
+++ doc_msgs.c
@@ -338,7 +338,7 @@ int i;
     my_printf ("readmessage:  no mh!\n");
     return (MNFERR);
   }
-  if ((int)p & 3) // This is appears to be testing for an address not aligned to 32-bit boundary. But why?
+  if ((intptr_t)p & 3) // This is appears to be testing for an address not aligned to 32-bit boundary. But why?
   {
     printf ("wtf?\r\n");
     return(MNFERR);
