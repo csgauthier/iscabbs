411e8563c5e88e43ed8aa853ef13ecd27a15fc0f
7jXlqS-hmpBUGsK <7jXlqS-hmpBUGsK@users.noreply.github.com>
2019-10-13 19:12:14 -0400

    removed custom strcasecmp

--- sysutil.c
+++ sysutil.c
@@ -725,11 +725,3 @@ errlog (const char *fmt, ...)
     return 0;
 }
 
-
-int
-strcasecmp(const char *s1, const char *s2)
-{
-  for (; *s1 && tolower(*s1) == tolower(*s2); s1++, s2++)
-    ;
-  return(*s1 || *s2);
-}
