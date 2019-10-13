01cdfbc71cc86230172d48a0d5737bc690f9133e
7jXlqS-hmpBUGsK <7jXlqS-hmpBUGsK@users.noreply.github.com>
2019-10-13 18:55:20 -0400

    errlog  takes a printf format

--- proto.h
+++ proto.h
@@ -67,7 +67,7 @@ void help (const char *, int);
 void more (const char *, int);
 int openfiles (void);
 char *mymmap (const char *, int *, int);
-int errlog (const char *,...);
+int errlog (const char * fmt,...) __attribute__((format(printf,1,2)));
 unsigned int sleep (unsigned int);
 int strcasecmp (const char *, const char *);
 
