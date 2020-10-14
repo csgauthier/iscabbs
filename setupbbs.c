3c635abd318ef87f5b8c6b73d66b56b90cd503a1
7jXlqS-hmpBUGsK <7jXlqS-hmpBUGsK@users.noreply.github.com>
2020-10-14 12:54:55 -0400

    setupbbs simplification
    - Added utility function `init_file` to create a zero-filled file
      without having to allocate large structures on the stack or repeated
      calls to `write`.
    
    - Use the BSD `err` API (available on Linux) instead of `perror`.
    
    - All setup calls except one are now implemented via the new `init_file`
      function.
    
    - The function setupmsgdata now allocate its init structure on the heap
      instead of the stack.

--- setupbbs.c
+++ setupbbs.c
@@ -1,168 +1,90 @@
-#include <unistd.h>
 #include "defs.h"
 #include "ext.h"
+#include <err.h>
+#include <unistd.h>
 
 #define XMSGSIZE 5000000
 
 char *zerobuf;
 size_t zerobuflen = 1048576;
 
-static void
-tick (void)
+/* Create a file with the given size filled with all zero bytes.
+ * Any existing data is overwritten. Exits immediately on failure. */
+static void init_file(const char* path, size_t size, mode_t mode)
 {
-    write (1, ".", 1);
+  // truncate the file to zero size, and create it if necessary.
+  int fd = open( path, O_RDWR | O_CREAT | O_TRUNC, mode);
+  if (fd == -1)
+      err( 1, "init_file: failed to open '%s' for writing", path);
+
+  // resize the file, filling it with zeros.
+  int rc = ftruncate( fd, size );
+  if (rc == -1)
+    err( 1, "init_file: ftruncate failed for file '%s'", path);
+
+  close(rc);
 }
 
 static void
 setupbtmp(void)
 {
-  int fd;
-  struct bigbtmp bigbtmp;
-
-  if ((fd = open (TMPDATA, O_RDWR | O_CREAT, 0660)) < 0)
-  {
-    perror ("can't open tmpdata");
-    return;
-  }
-
-  bzero (&bigbtmp, sizeof (struct bigbtmp));
-  write (fd, (char *)&bigbtmp, sizeof (struct bigbtmp));
-  close (fd);
-  printf ("btmp setup complete\n");
+  init_file( TMPDATA, sizeof(struct bigbtmp), 0660);
+  printf ("btmp setup complete.\n");
 }
 
 static void
 setupmsgmain(void)
 {
-  int fd;
-  long i;
-
-  if ((fd = open (MSGMAIN, O_WRONLY | O_CREAT, 0660)) < 0)
-  {
-    perror ("can't open msgmain");
-    return;
-  }
-
-  for (i = MM_FILELEN; i > 0;) {
-    size_t res;
-    res = write (fd, zerobuf,
-		 ((zerobuflen) > i ? i : zerobuflen));
-    if (res <= 0) {
-	perror ("writing msgmain");
-	return;
-    }
-    i -= res;
-    tick ();
-  }
-  close (fd);
-  printf ("msgmain done\n");
+  init_file( MSGMAIN, MM_FILELEN, 0660);
+  printf ("msgmain setup complete.\n");
 }
-
 
 static void
 setupmsgdata(void)
 {
-  int fd;
-  struct msg msg;
+  // initialize struct msg with non-zero data.
+  struct msg * msg = (struct msg*) calloc(1, sizeof(struct msg));
+  msg->xmsgsize = XMSGSIZE;
 
-  if ((fd = open (MSGDATA, O_RDWR | O_CREAT, 0660)) < 0) {
-    perror ("can't open msgdata");
-    return;
-  }
+  int fd = open( MSGDATA, O_RDWR | O_CREAT | O_TRUNC, 0660);
+  if (fd == -1)
+      err( 1, "setupmsgdata: failed to open '%s' for writing", MSGDATA);
 
-  bzero ((char *) &msg, sizeof (struct msg));
-  msg.xmsgsize = XMSGSIZE;
-  write (fd, (char *)&msg, sizeof (struct msg));
-  close (fd);
-  printf ("msg data done\n");
+  int rc = write( fd, msg, sizeof(struct msg));
+  if (rc == -1)
+    err( 1, "setupmsgdata: failed to write file '%s'", MSGDATA);
+
+  close(fd);
+  free(msg);
+  printf ("msg data setup complete\n");
 }
-
 
 static void
 setupuserdata(void)
 {
-  int fd;
-  int i;
-
-  if ((fd = open (USERDATA, O_WRONLY | O_CREAT, 0660)) < 0)
-  {
-    perror ("can't open userdata");
-    return;
-  }
-
-  i = sizeof (struct userdata) + sizeof (struct user) * MAXTOTALUSERS;
-  while (i > 0) {
-    size_t res;
-    res = write (fd, zerobuf,
-		 ((zerobuflen) > i ? i : zerobuflen));
-    if (res <= 0) {
-	perror ("writing msgmain");
-	return;
-    }
-    tick ();
-    i -= res;
-  }
-  close (fd);
-
-  printf ("userdata done\n");
+  init_file( USERDATA, sizeof (struct userdata) + sizeof (struct user) * MAXTOTALUSERS, 0660);
+  printf ("userdata setup complete\n");
 }
 
 
 static void
 setupxmsgdata(void)
 {
-  int fd;
-  int i;
-
-  if ((fd = open (XMSGDATA, O_WRONLY | O_CREAT, 0660)) < 0)
-  {
-    perror ("can'topen xmsgdata");
-    return;
-  }
-
-  i = XMSGSIZE;
-  while (i > 0) {
-    size_t res;
-    res = write (fd, zerobuf,
-		 ((zerobuflen) > i ? i : zerobuflen));
-    if (res <= 0) {
-	perror ("writing msgmain");
-	return;
-    }
-    tick ();
-    i -= res;
-  }
-  close (fd);
-
-  printf ("xmsgdata done\n");
+  init_file( XMSGDATA, XMSGSIZE, 0660);
+  printf ("xmsgdata setup complete\n");
 }
 
 static void
 setupvoteinfo(void)
 {
-  struct voteinfo voteinfo;
-  int fd;
-
-  bzero (&voteinfo, sizeof (struct voteinfo));
-  if ((fd = open (VOTEFILE, O_RDWR | O_CREAT, 0664)) < 0)
-  {
-    perror ("voteinfo");
-    return;
-  }
-
-  write (fd, (char*)& voteinfo, sizeof (struct voteinfo));
-  close (fd);
-  printf ("Voteinfo done\n");
+  init_file( VOTEFILE, sizeof(struct voteinfo), 0664);
+  printf ("voteinfo setup complete\n");
 }
 
 
 int
 main(int argc, char *argv[])
 {
-  if ((zerobuf = calloc (zerobuflen, 1)) == NULL) {
-    perror ("Couldn't allocate zero buffer");
-    exit (1);
-  }
   setupbtmp();
   setupmsgmain();
   setupmsgdata();
